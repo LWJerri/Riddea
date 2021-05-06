9b64c4788fbe85c846f3bd2f13d9e1686276a148
LWJerri <CLWJerri@yandex.ua>
2021-05-06 18:10:39 +0300

    Add ready event trigger

---
+++ src/events/ready.ts
@@ -0,0 +1,24 @@
+import { createConnection } from "typeorm";
+import { bot } from "../app";
+import { Settings } from "../entities/Settings";
+
+export default async function readyEvent() {
+    console.log(` > ${bot.botInfo.username} ready!`);
+
+    const connection = await createConnection();
+    connection.getRepository(Settings).create();
+
+    if (!(await connection.getRepository(Settings).findOne(1))) {
+        const setting = new Settings();
+        setting.avatarUsed = 0;
+        setting.bondageUsed = 0;
+        setting.hentaiUsed = 0;
+        setting.thighsUsed = 0;
+        setting.uploadUsed = 0;
+        setting.wallpaperUsed = 0;
+
+        await connection.manager.save(setting);
+    }
+
+    await connection.close();
+}
