031644861c0f0363506b1b269b84714c23fbaa00
LWJerri <CLWJerri@yandex.ua>
2021-05-07 17:12:30 +0300

    Modify thighs command

---
+++ src/commands/thighs.ts
@@ -0,0 +1,37 @@
+import { Context } from "telegraf";
+import { bot, fileType } from "../app";
+import axios from "axios";
+import { createConnection, getConnection } from "typeorm";
+import { Settings } from "../entities/Settings";
+
+export async function thighsCMD(message: Context) {
+    const output = await (
+        await axios.get("https://shiro.gg/api/images/nsfw/thighs")
+    ).data;
+
+    if (!fileType.includes(output.fileType)) return;
+
+    await bot.telegram.sendPhoto(message.message.chat.id, output.url, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "Show new thighs image",
+                        callback_data: "NEW_THIGHS",
+                    },
+                ],
+            ],
+        },
+    });
+
+    if (getConnection().isConnected) return;
+
+    const connection = await createConnection();
+    const dbRepo = connection.getRepository(Settings);
+    const dbRepoUpdate = await dbRepo.findOne(1);
+    dbRepoUpdate.thighsUsed = dbRepoUpdate.thighsUsed + 1;
+    await dbRepo.save(dbRepoUpdate);
+    await connection.close();
+
+    return;
+}
