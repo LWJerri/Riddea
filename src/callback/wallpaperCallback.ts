66bf83999861d41248a43443cc7348561e7c305a
LWJerri <CLWJerri@yandex.ua>
2021-05-07 17:01:27 +0300

    Add wallpaper callback

---
+++ src/callback/wallpaperCallback.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { bot, fileType } from "../app";
+
+export async function wallpaperCallback(callback: any) {
+    const output = await (
+        await axios.get("https://shiro.gg/api/images/wallpapers")
+    ).data;
+
+    if (!fileType.includes(output.fileType)) return;
+
+    await bot.telegram.sendPhoto(
+        callback.update.callback_query.message.chat.id,
+        output,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: "Show new wallpaper image",
+                            callback_data: "NEW_WALLPAPER",
+                        },
+                    ],
+                ],
+            },
+        }
+    );
+}
