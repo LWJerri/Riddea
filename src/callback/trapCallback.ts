a76c863db18f3dec74769c840c13198087300d97
LWJerri <CLWJerri@yandex.ua>
2021-05-07 17:01:17 +0300

    Add trap callback

---
+++ src/callback/trapCallback.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { bot, fileType } from "../app";
+
+export async function trapCallback(callback: any) {
+    const output = await (await axios.get("https://shiro.gg/api/images/trap"))
+        .data;
+
+    if (!fileType.includes(output.fileType)) return;
+
+    await bot.telegram.sendPhoto(
+        callback.update.callback_query.message.chat.id,
+        output,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: "Show new trap image",
+                            callback_data: "NEW_TRAP",
+                        },
+                    ],
+                ],
+            },
+        }
+    );
+}
