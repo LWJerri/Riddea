8a0d523fec2ba2927e18ca8eb7eec5a928f538d0
LWJerri <CLWJerri@yandex.ua>
2021-05-07 17:14:36 +0300

    Modify wallpaper command

---
+++ src/commands/wallpaper.ts
@@ -0,0 +1,37 @@
+import { Context } from "telegraf";
+import { bot, fileType } from "../app";
+import axios from "axios";
+import { createConnection, getConnection } from "typeorm";
+import { Settings } from "../entities/Settings";
+
+export async function wallpaperCMD(message: Context) {
+    const output = await (
+        await axios.get("https://shiro.gg/api/images/wallpapers")
+    ).data;
+
+    if (!fileType.includes(output.fileType)) return;
+
+    await bot.telegram.sendPhoto(message.message.chat.id, output.url, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "Show new wallpaper image",
+                        callback_data: "NEW_WALLPAPER",
+                    },
+                ],
+            ],
+        },
+    });
+
+    if (getConnection().isConnected) return;
+
+    const connection = await createConnection();
+    const dbRepo = connection.getRepository(Settings);
+    const dbRepoUpdate = await dbRepo.findOne(1);
+    dbRepoUpdate.wallpaperUsed = dbRepoUpdate.wallpaperUsed + 1;
+    await dbRepo.save(dbRepoUpdate);
+    await connection.close();
+
+    return;
+}
