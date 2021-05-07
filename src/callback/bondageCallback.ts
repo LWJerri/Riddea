ab6d83ea6a6e79298d86460b558ec3bedbfd56f0
LWJerri <CLWJerri@yandex.ua>
2021-05-07 17:00:15 +0300

    Add bondage callback

---
+++ src/callback/bondageCallback.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { bot, fileType } from "../app";
+
+export async function bondageCallback(callback: any) {
+    const output = await (
+        await axios.get("https://shiro.gg/api/images/nsfw/bondage")
+    ).data;
+
+    if (!fileType.includes(output.fileType)) return;
+
+    await bot.telegram.sendPhoto(
+        callback.update.callback_query.message.chat.id,
+        output,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: "Show new bondage image",
+                            callback_data: "NEW_BONDAGE",
+                        },
+                    ],
+                ],
+            },
+        }
+    );
+}
