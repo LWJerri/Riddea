f80eff7aa0907431e23081538093629d8cb7d491
LWJerri <CLWJerri@yandex.ua>
2021-05-07 18:11:56 +0300

    Add ready event listener

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -10,13 +10,6 @@ export async function readyEvent() {
 
     if (!(await connection.getRepository(Settings).findOne(1))) {
         const setting = new Settings();
-        setting.avatarUsed = 0;
-        setting.bondageUsed = 0;
-        setting.hentaiUsed = 0;
-        setting.thighsUsed = 0;
-        setting.uploadUsed = 0;
-        setting.wallpaperUsed = 0;
-
         await connection.manager.save(setting);
     }
 
