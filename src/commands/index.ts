5bd6a387f9dbdc8e91a822fa79f35a6416a916bb
LWJerri <CLWJerri@yandex.ua>
2021-05-09 21:38:26 +0300

    Rewite index command file

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,25 +1,11 @@
-import avatarCMD from "./avatar";
-import bondageCMD from "./bondage";
-import helpCMD from "./help";
-import hentaiCMD from "./hentai";
-import nekoCMD from "./neko";
-import startCMD from "./start";
-import statusCMD from "./status";
-import thighsCMD from "./thighs";
-import trapCMD from "./trap";
-import uploadCMD from "./upload";
-import wallpaperCMD from "./wallpaper";
-
-export {
-    avatarCMD,
-    bondageCMD,
-    helpCMD,
-    hentaiCMD,
-    nekoCMD,
-    startCMD,
-    statusCMD,
-    thighsCMD,
-    trapCMD,
-    uploadCMD,
-    wallpaperCMD,
-};
+export { default as avatarCmd } from "./avatar";
+export { default as bondageCMD } from "./bondage";
+export { default as helpCMD } from "./help";
+export { default as hentaiCMD } from "./hentai";
+export { default as nekoCMD } from "./neko";
+export { default as startCMD } from "./start";
+export { default as statusCMD } from "./status";
+export { default as thighsCMD } from "./thighs";
+export { default as trapCMD } from "./trap";
+export { default as uploadCMD } from "./upload";
+export { default as wallpaperCMD } from "./wallpaper";
--- src/entities/Settings.ts
+++ src/entities/Settings.ts
@@ -5,27 +5,27 @@ export class Settings {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column("int", { default: 0 })
+    @Column({ default: 0 })
     avatarUsed: number;
 
-    @Column("int", { default: 0 })
+    @Column({ default: 0 })
     bondageUsed: number;
 
-    @Column("int", { default: 0 })
+    @Column({ default: 0 })
     hentaiUsed: number;
 
-    @Column("int", { default: 0 })
+    @Column({ default: 0 })
     nekoUsed: number;
 
-    @Column("int", { default: 0 })
+    @Column({ default: 0 })
     thighsUsed: number;
 
-    @Column("int", { default: 0 })
+    @Column({ default: 0 })
     trapUsed: number;
 
-    @Column("int", { default: 0 })
+    @Column({ default: 0 })
     uploadUsed: number;
 
-    @Column("int", { default: 0 })
+    @Column({ default: 0 })
     wallpaperUsed: number;
 }
