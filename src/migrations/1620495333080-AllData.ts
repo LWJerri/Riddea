90896c0fa8fc39208386b8ded92bf250030dc9f8
LWJerri <CLWJerri@yandex.ua>
2021-05-09 20:46:02 +0300

    Remove missed console.log()

--- src/migrations/1620495333080-AllData.ts
+++ src/migrations/1620495333080-AllData.ts
@@ -16,18 +16,12 @@ export class AllData1620495333080 implements MigrationInterface {
             await queryRunner.query(
                 `CREATE TABLE "settings" ("id" SERIAL NOT NULL, "avatarUsed" integer NOT NULL DEFAULT '0', "bondageUsed" integer NOT NULL DEFAULT '0', "hentaiUsed" integer NOT NULL DEFAULT '0', "nekoUsed" integer NOT NULL DEFAULT '0', "thighsUsed" integer NOT NULL DEFAULT '0', "trapUsed" integer NOT NULL DEFAULT '0', "uploadUsed" integer NOT NULL DEFAULT '0', "wallpaperUsed" integer NOT NULL DEFAULT '0', CONSTRAINT "PK_0669fe20e252eb692bf4d344975" PRIMARY KEY ("id"))`
             );
-            console.log("setting created");
-        } else {
-            console.log("setting exist");
         }
 
         if (!tableUpload.length) {
             await queryRunner.query(
                 `CREATE TABLE "upload" ("id" SERIAL NOT NULL, "userID" integer NOT NULL, "storage" text array NOT NULL, CONSTRAINT "PK_1fe8db121b3de4ddfa677fc51f3" PRIMARY KEY ("id"))`
             );
-            console.log("upload created");
-        } else {
-            console.log("upload exist");
         }
     }
 
