94c5b8bbe55715a307614bcea472354c0aa4f453
LWJerri <CLWJerri@yandex.ua>
2021-05-09 21:29:28 +0300

    Add index file for commands

---
+++ src/commands/index.ts
@@ -0,0 +1,25 @@
+import avatarCMD from "./avatar";
+import bondageCMD from "./bondage";
+import helpCMD from "./help";
+import hentaiCMD from "./hentai";
+import nekoCMD from "./neko";
+import startCMD from "./start";
+import statusCMD from "./status";
+import thighsCMD from "./thighs";
+import trapCMD from "./trap";
+import uploadCMD from "./upload";
+import wallpaperCMD from "./wallpaper";
+
+export {
+    avatarCMD,
+    bondageCMD,
+    helpCMD,
+    hentaiCMD,
+    nekoCMD,
+    startCMD,
+    statusCMD,
+    thighsCMD,
+    trapCMD,
+    uploadCMD,
+    wallpaperCMD,
+};
