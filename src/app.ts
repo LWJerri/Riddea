a813e581621f075d9f9c34d5639276f8893feb40
Satont <satontworldwide@gmail.com>
2021-05-12 17:16:52 +0300

    Update prettier things

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { Telegraf } from "telegraf";
 import callbackEvent from "./events/callback";
 import readyEvent from "./events/ready";
 import { createConnection } from "typeorm";
-import { promises as fs } from 'fs';
+import { promises as fs } from "fs";
 import { resolve } from "path";
 
 export const bot = new Telegraf(process.env.TOKEN);
@@ -13,13 +13,15 @@ export const bot = new Telegraf(process.env.TOKEN);
 bot.on("callback_query", callbackEvent);
 
 async function bootstrap() {
-    const commandsDirPath = resolve(__dirname, 'commands')
-    const cmds = (await fs.readdir(commandsDirPath, { withFileTypes: true })).map(f => f.name)
+    const commandsDirPath = resolve(__dirname, "commands");
+    const cmds = (
+        await fs.readdir(commandsDirPath, { withFileTypes: true })
+    ).map((f) => f.name);
     for (const command of cmds) {
-        const file = (await import(resolve(commandsDirPath, command))).default
-        const commandName = command.split('.')[0]
-        bot.command(commandName, file)
-        console.info(`Command ${commandName} loaded`)
+        const file = (await import(resolve(commandsDirPath, command))).default;
+        const commandName = command.split(".")[0];
+        bot.command(commandName, file);
+        console.info(`Command ${commandName} loaded`);
     }
 
     await createConnection();
