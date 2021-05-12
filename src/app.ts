64024fc62d5c72926e5abf2f2d9eb779dc476418
Satont <satontworldwide@gmail.com>
2021-05-12 17:16:36 +0300

    Dynamically load commands

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,24 @@ import "reflect-metadata";
 import { Telegraf } from "telegraf";
 import callbackEvent from "./events/callback";
 import readyEvent from "./events/ready";
-import * as commands from "./commands";
 import { createConnection } from "typeorm";
+import { promises as fs } from 'fs';
+import { resolve } from "path";
 
 export const bot = new Telegraf(process.env.TOKEN);
 
 bot.on("callback_query", callbackEvent);
-bot.command("avatar", commands.avatarCmd);
-bot.command("bondage", commands.bondageCMD);
-bot.command("help", commands.helpCMD);
-bot.command("hentai", commands.hentaiCMD);
-bot.command("neko", commands.nekoCMD);
-bot.command("start", commands.startCMD);
-bot.command("status", commands.statusCMD);
-bot.command("thighs", commands.thighsCMD);
-bot.command("trap", commands.trapCMD);
-bot.command("upload", commands.uploadCMD);
-bot.command("wallpaper", commands.wallpaperCMD);
 
 async function bootstrap() {
+    const commandsDirPath = resolve(__dirname, 'commands')
+    const cmds = (await fs.readdir(commandsDirPath, { withFileTypes: true })).map(f => f.name)
+    for (const command of cmds) {
+        const file = (await import(resolve(commandsDirPath, command))).default
+        const commandName = command.split('.')[0]
+        bot.command(commandName, file)
+        console.info(`Command ${commandName} loaded`)
+    }
+
     await createConnection();
     await bot.launch();
     await readyEvent();
