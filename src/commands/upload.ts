677f9a94806b31d3cd70fffb0c0c85e9f76cfc61
LWJerri <CLWJerri@yandex.ua>
2021-05-14 21:35:24 +0300

    Clear unused code

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,22 +1,7 @@
-import { createConnection } from "typeorm";
 import { Context } from "telegraf";
-import { getConnection } from "typeorm";
-import { Upload } from "../entities/Upload";
 
 export default async function uploadCMD(message: Context) {
     await message.reply("Oops! Sorry, this command currently not available!");
 
     return;
-    /*if (getConnection().isConnected) return;
-
-    const connection = await createConnection();
-    const user = connection.manager.getRepository(Upload);
-
-    if (!(await user.find({ userID: message.from.id })).length) {
-        const upload = new Upload();
-        upload.userID = message.from.id;
-        upload.storage = [];
-        await connection.manager.save(upload);
-        await connection.close();
-    }*/
 }
