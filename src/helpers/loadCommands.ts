139440d331402fbb5d6a4b33abab6b616879bc53
LWJerri <CLWJerri@yandex.ua>
2021-05-14 23:15:47 +0300

    Ignore files with .d.ts ext

--- src/helpers/loadCommands.ts
+++ src/helpers/loadCommands.ts
@@ -9,7 +9,7 @@ export async function loadCommands() {
     const commandsDirPath = resolve(__dirname, "..", "commands");
     const cmds = (await fs.readdir(commandsDirPath, { withFileTypes: true }))
         .map((f) => f.name)
-        .filter((name) => !name.includes("index"));
+        .filter((name) => !name.includes("index") && !name.includes(".d.ts"));
 
     for (const command of cmds) {
         const file = await import(resolve(commandsDirPath, command));
