3d1b3f7463f2a7ac3ad516b68f252719675fbd8e
LWJerri <CLWJerri@yandex.ua>
2021-05-14 21:15:50 +0300

    Update entity name, add axios response checks and bot callback answer

--- src/callback/bondageCallback.ts
+++ src/callback/bondageCallback.ts
@@ -1,15 +1,27 @@
 import axios from "axios";
 import { getRepository } from "typeorm";
 import { bot } from "../app";
-import { Settings } from "../entities/Settings";
+import { Statistic } from "../entities/Statistic";
 import { fileTypes } from "../constants";
 
 export default async function bondageCallback(callback: any) {
-    const output = await (
-        await axios.get("https://shiro.gg/api/images/nsfw/bondage")
-    ).data;
+    const url = await axios
+        .get("https://shiro.gg/api/images/nsfw/bondage")
+        .catch(() => null);
 
-    if (!fileTypes.includes(output.fileType)) return;
+    if (!url)
+        return await bot.telegram.sendMessage(
+            callback.update.callback_query.message.chat.id,
+            "Oops! Can't get response from API :c"
+        );
+
+    const output = url.data;
+
+    if (!fileTypes.includes(output.fileType))
+        return await bot.telegram.sendMessage(
+            callback.update.callback_query.message.chat.id,
+            "Oops! Sometimes I can't send you an image and now it's this moment. Please, repeat your command (~‾▿‾)~"
+        );
 
     await bot.telegram.sendPhoto(
         callback.update.callback_query.message.chat.id,
@@ -28,6 +40,8 @@ export default async function bondageCallback(callback: any) {
         }
     );
 
-    await getRepository(Settings).increment({ id: 1 }, "bondageUsed", 1);
+    await bot.telegram.answerCbQuery(callback.update.callback_query.id);
+
+    await getRepository(Statistic).increment({ id: 1 }, "bondageUsed", 1);
     return;
 }
