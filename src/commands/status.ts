72a9dbf5c8d486f27395fe15c7c0277b7910d6e2
LWJerri <CLWJerri@yandex.ua>
2021-05-14 21:35:08 +0300

    Rename entity Settings to Statistic and fix uptime time

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,6 +1,6 @@
 import { Context } from "telegraf";
 import { getRepository } from "typeorm";
-import { Settings } from "../entities/Settings";
+import { Statistic } from "../entities/Statistic";
 import { Upload } from "../entities/Upload";
 import humanize from "humanize-duration";
 import { resolve } from "path";
@@ -8,16 +8,16 @@ import { resolve } from "path";
 const pkg = require(resolve(process.cwd(), "package.json"));
 
 export default async function statusCMD(message: Context) {
-    const uptime = humanize(Date.now() - Math.floor(process.uptime()) * 1000, {
+    const uptime = humanize(Math.floor(process.uptime()) * 1000, {
         round: true,
         language: "en",
     });
 
-    const settingTable = await getRepository(Settings).findOne(1);
+    const statisticTable = await getRepository(Statistic).findOne(1);
     const uploadTable = await getRepository(Upload).find();
 
     await message.reply(
-        `COMMANDS STATS:\n> /avatar command used ${settingTable.avatarUsed} times.\n> /bondage command used ${settingTable.bondageUsed} times.\n> /hentai command used ${settingTable.hentaiUsed} times.\n> /neko command used ${settingTable.nekoUsed} times.\n> /thighs command used ${settingTable.thighsUsed} times.\n> /trap command used ${settingTable.trapUsed} times.\n> /upload command used ${settingTable.uploadUsed} times.\n> /wallpaper command used ${settingTable.wallpaperUsed} times.`
+        `COMMANDS STATS:\n> /avatar command used ${statisticTable.avatarUsed} times.\n> /bondage command used ${statisticTable.bondageUsed} times.\n> /hentai command used ${statisticTable.hentaiUsed} times.\n> /neko command used ${statisticTable.nekoUsed} times.\n> /thighs command used ${statisticTable.thighsUsed} times.\n> /trap command used ${statisticTable.trapUsed} times.\n> /upload command used ${statisticTable.uploadUsed} times.\n> /wallpaper command used ${statisticTable.wallpaperUsed} times.`
     );
     await message.reply(
         `UPLOADS STATS:\nUploaded ${uploadTable.length} images!`
