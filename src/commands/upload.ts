bf2008928ed7ff0c4c4c9c1a0675113c635e53b4
LWJerri <CLWJerri@yandex.ua>
2021-05-24 19:26:53 +0300

    Finish /upload command

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,11 +1,11 @@
-import { Context } from "telegraf";
+import { getRepository } from "typeorm";
+import { Statistic } from "../entities/Statistic";
 
-export const description = "Upload you image to bot database";
+export const description = "Upload your picture to database";
 
-export default async function uploadCMD(message: Context) {
-    await message
-        .reply("Oops! Sorry, this command currently not available!")
-        .catch(() => {});
+export default async function uploadCMD(message: any) {
+    await message.scene.enter("upload");
 
+    await getRepository(Statistic).increment({ id: 1 }, "uploadUsed", 1);
     return;
 }
