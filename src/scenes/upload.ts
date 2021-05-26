03d322042a2d462bf90caf69fc63dba8f294ff49
LWJerri <CLWJerri@yandex.ua>
2021-05-26 23:11:50 +0300

    Add catch output error - upload.ts

--- src/scenes/upload.ts
+++ src/scenes/upload.ts
@@ -2,11 +2,10 @@ import { Scenes, Markup, Context } from "telegraf";
 import { getRepository } from "typeorm";
 import { Upload } from "../entities/Upload";
 
-const tempData = new Map();
 export const uploadScene = new Scenes.BaseScene<Scenes.SceneContext>("upload")
-    .enter((msg: Context) => msg.reply(`Okay, send me your image!`).catch(() => {}))
-    .on("photo", async (msg) => {
-        await msg
+    .enter((ctx: Context) => ctx.reply(`Okay, send me your image!`).catch((err: any) => console.log("[ERROR]: ", err)))
+    .on("photo", async (ctx) => {
+        await ctx
             .reply(
                 "Do you wanna upload this photo?",
                 Markup.inlineKeyboard([
@@ -22,29 +21,33 @@ export const uploadScene = new Scenes.BaseScene<Scenes.SceneContext>("upload")
                     ],
                 ])
             )
-            .catch(() => {});
+            .catch((err: any) => console.log("[ERROR]: ", err));
 
-        tempData.set(msg.message.from.id, msg.message.photo.pop().file_id);
+        (ctx.scene.session as any).fileID = ctx.message.photo.pop().file_id;
     })
-    .on("message", async (msg) => {
-        await msg.reply(`If you don't want upload image, type /cancel!`).catch(() => {});
+    .command("cancel", async (ctx) => {
+        await ctx.scene.leave().catch((err: any) => console.log("[ERROR]: ", err));
+        await ctx.reply("You leave from upload image section!").catch((err: any) => console.log("[ERROR]: ", err));
     })
-    .action("PUBLISH", async (msg) => {
-        await msg.answerCbQuery().catch(() => {});
+    .on("message", async (ctx) => {
+        await ctx.reply(`If you don't want upload image, type /cancel!`).catch((err: any) => console.log("[ERROR]: ", err));
+    })
+    .action("PUBLISH", async (ctx) => {
+        await ctx.answerCbQuery().catch((err: any) => console.log("[ERROR]: ", err));
 
         const uploadTable = getRepository(Upload);
         const newUpload = new Upload();
-        newUpload.userID = msg.from.id;
-        newUpload.fileID = tempData.get(msg.from.id);
+        newUpload.userID = ctx.from.id;
+        newUpload.fileID = (ctx.scene.session as any).fileID;
         await uploadTable.save(newUpload);
-        await msg.reply(`Yay, your image loaded to bot database!`).catch(() => {});
-
-        tempData.delete(msg.from.id);
-        await msg.scene.leave();
+        await ctx.reply(`Yay, your image loaded to bot database!`).catch((err: any) => console.log("[ERROR]: ", err));
+        await ctx.scene.leave().catch((err: any) => console.log("[ERROR]: ", err));
     })
-    .action("CANCEL", async (msg) => {
-        await msg.answerCbQuery().catch(() => {});
-        await msg.reply(`Woop, your image removed from the queue to loading in the database!`).catch(() => {});
-        tempData.delete(msg.from.id);
-        await msg.scene.leave();
+    .action("CANCEL", async (ctx) => {
+        await ctx.answerCbQuery().catch((err: any) => console.log("[ERROR]: ", err));
+
+        await ctx
+            .reply(`Woop, your image removed from the queue to loading in the database!`)
+            .catch((err: any) => console.log("[ERROR]: ", err));
+        await ctx.scene.leave().catch((err: any) => console.log("[ERROR]: ", err));
     });
