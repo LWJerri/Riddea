8b188df9bc574ca56c51f696e5bf809d5c4356d4
Satont <satontworldwide@gmail.com>
2021-05-26 15:35:38 +0300

    upd

--- ormconfig.ts
+++ ormconfig.ts
@@ -24,7 +24,7 @@ module.exports = {
         entitiesDir: "src/entities",
         migrationsDir: "src/migrations",
     },
-    /* ssl: {
+    ssl: {
         rejectUnauthorized: false,
-    }, */
+    },
 };
