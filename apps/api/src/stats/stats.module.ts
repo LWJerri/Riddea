0c543608f220caf731f848b87123d785d4b984f1
Satont <satontworldwide@gmail.com>
2021-06-06 19:32:56 +0300

    Rewrite ClientModule client initialization
    fixes #56

--- apps/api/src/stats/stats.module.ts
+++ apps/api/src/stats/stats.module.ts
@@ -1,34 +1,21 @@
 import { CacheModule, Module } from "@nestjs/common";
-import { ClientsModule, TcpClientOptions, Transport } from "@nestjs/microservices";
+import { ClientsModule } from "@nestjs/microservices";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Statistic, Upload } from "@riddea/typeorm";
-import { resolve } from "path";
 import { StatsController } from "./stats.controller";
 import { StatsService } from "./stats.service";
-import { readFile } from "fs/promises";
-import dotenv from "dotenv";
 import isDocker from "is-docker";
 
 @Module({
     imports: [
-        ClientsModule.registerAsync([
+        ClientsModule.register([
             {
-                name: "BOT",
-                useFactory: async (): Promise<TcpClientOptions> => {
-                    let envFile: dotenv.DotenvParseOutput = null;
-                    try {
-                        envFile = dotenv.parse(await readFile(resolve(process.cwd(), "..", "bot", ".env"), { encoding: "utf-8" }));
-                    } catch {}
-                    const port = Number(envFile?.port ?? 3001);
-                    return {
-                        transport: Transport.TCP,
-                        options: {
-                            host: isDocker() ? "bot" : process.env.BOT_SERVICE_HOST ?? "localhost",
-                            port,
-                        },
-                    };
-                },
-            },
+                name: 'BOT',
+                options: {
+                    host: isDocker() ? "bot" : process.env.BOT_SERVICE_HOST ?? "localhost",
+                    port: Number(process.env.BOT_PORT ?? 3001),
+                }
+            }
         ]),
         TypeOrmModule.forFeature([Statistic, Upload]),
         CacheModule.register({
