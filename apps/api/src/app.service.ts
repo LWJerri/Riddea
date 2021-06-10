ad73d0510032163db0937b7998c9332525e86d7a
LWJerri <CLWJerri@yandex.ua>
2021-06-10 12:00:02 +0300

    Add API endpoints list in main response

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
+const enpointsList = [
+  { v1: ["/collections/:id/", "/collection/:id/images?page=:page&limit=:limit", "/stats", "/users/:userId/collections"] },
+];
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
+  getHello(): {} {
+    return enpointsList;
   }
 }
