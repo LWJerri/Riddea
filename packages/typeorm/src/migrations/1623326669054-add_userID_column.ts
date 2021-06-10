ae47bbf668408e4843170afe8ca8426e24bd08db
LWJerri <CLWJerri@yandex.ua>
2021-06-10 15:12:20 +0300

    Prettier rules

--- packages/typeorm/src/migrations/1623326669054-add_userID_column.ts
+++ packages/typeorm/src/migrations/1623326669054-add_userID_column.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class addUserIDColumn1623326669054 implements MigrationInterface {
-    name = 'addUserIDColumn1623326669054'
+  name = "addUserIDColumn1623326669054";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "statistic" ADD "userID" integer NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "statistic" ADD "userID" integer NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "statistic" DROP COLUMN "userID"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "statistic" DROP COLUMN "userID"`);
+  }
 }
