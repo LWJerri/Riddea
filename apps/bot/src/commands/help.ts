1260aaac230a9dd0e2e30d3a4d0cce2fd10ececf
LWJerri <CLWJerri@yandex.ua>
2021-06-10 17:24:17 +0300

    Update /help command

--- apps/bot/src/commands/help.ts
+++ apps/bot/src/commands/help.ts
@@ -12,22 +12,25 @@ export default class extends CommandInterface {
   }
 
   async run(ctx: Context) {
-    await ctx.reply(`Yo! Type / for view list of all bot commands.`, {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: "Sponsors", callback_data: "SEND_SPONSORS" }],
-          [
-            {
-              text: "GitHub",
-              url: "https://github.com/LWJerri/Riddea",
-            },
-            {
-              text: "Support me",
-              url: "https://www.donationalerts.com/r/lwjerri",
-            },
+    await ctx.reply(
+      `Yo! Type / for view list of all bot commands.\n\nNOTE: If you wanna upload picture, you need send this picture to bot without commands, compression, and etc. Also you can forward picture from other chat :3`,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Sponsors", callback_data: "SEND_SPONSORS" }],
+            [
+              {
+                text: "GitHub",
+                url: "https://github.com/LWJerri/Riddea",
+              },
+              {
+                text: "Support me",
+                url: "https://www.donationalerts.com/r/lwjerri",
+              },
+            ],
           ],
-        ],
+        },
       },
-    });
+    );
   }
 }
