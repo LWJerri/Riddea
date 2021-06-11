cc95e0c7514714002d1059b1e6daa758746a4cad
Satont <satontworldwide@gmail.com>
2021-06-11 05:55:25 +0300

    remove unused files

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { CollectionsModule } from "./collections/collections.module";
 import { UsersModule } from "./users/users.module";
 import { StatsModule } from "./stats/stats.module";
