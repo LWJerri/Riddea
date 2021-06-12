e323bb016a1f04ba7fb623d015ecf167abc06c91
Satont <satontworldwide@gmail.com>
2021-06-12 11:44:41 +0300

    Add documentation about response headers

--- apps/api/src/collections/collections.controller.ts
+++ apps/api/src/collections/collections.controller.ts
@@ -2,7 +2,7 @@ import { CacheInterceptor, Controller, Get, Param, Query, Res, UseInterceptors, 
 import { CollectionsService } from "./collections.service";
 import { GetCollectionImages } from "./validations/getCollectionImages";
 import { FastifyReply } from "fastify";
-import { ApiForbiddenResponse, ApiResponse } from "@nestjs/swagger";
+import { ApiForbiddenResponse, ApiHeader, ApiResponse } from "@nestjs/swagger";
 import { CollectionDTO, CollectionUploadsDTO } from "./dto/collection.dto";
 
 @Controller("/v1/collections")
@@ -28,6 +28,15 @@ export class CollectionsController {
     status: 200,
     description: "The found images",
     type: () => CollectionUploadsDTO,
+    headers: {
+      total: {
+        example: 100,
+        description: "Total images by query.",
+        schema: {
+          type: "number",
+        },
+      },
+    },
   })
   @ApiForbiddenResponse({ status: 403, description: "Collection is private" })
   async getCollectionImages(@Query() query: GetCollectionImages, @Param("id") id: string, @Res() res: FastifyReply) {
--- apps/api/src/collections/dto/collection.dto.ts
+++ apps/api/src/collections/dto/collection.dto.ts
@@ -3,6 +3,7 @@ import { PartialType, OmitType, ApiProperty } from "@nestjs/swagger";
 import { UploadsDTO } from "./upload.dto";
 
 export class CollectionDTO extends PartialType(OmitType(Collection, ["uploads"] as const)) {}
+
 export class CollectionUploadsDTO {
   @ApiProperty({ type: Boolean, example: false })
   nextPage: boolean;
