d12bfb564e97fec5adc066a0dcb8f22a35d7cd04
LWJerri <CLWJerri@yandex.ua>
2021-06-16 17:23:20 +0300

    Update wallpaper command

--- apps/bot/src/commands/wallpaper.ts
+++ apps/bot/src/commands/wallpaper.ts
@@ -13,21 +13,17 @@ export default class extends CommandInterface {
   }
 
   async run(ctx: Context) {
-    try {
-      const images = await shiroApi({ endPoint: "wallpapers", amount: 10 });
+    const images = await shiroApi({ endPoint: "wallpapers", amount: 10 });
 
-      await ctx.replyWithMediaGroup(
-        images.map((image) => {
-          return {
-            type: "photo",
-            media: image.url,
-          };
-        }),
-      );
+    await ctx.replyWithMediaGroup(
+      images.map((image) => {
+        return {
+          type: "photo",
+          media: image.url,
+        };
+      }),
+    );
 
-      await ctx.reply("Do you like to see more wallpapers?", Markup.inlineKeyboard([Markup.button.callback("Give me more!", this.action)]));
-    } catch (e) {
-      console.error(e);
-    }
+    await ctx.reply("Do you like to see more wallpapers?", Markup.inlineKeyboard([Markup.button.callback("Give me more!", this.action)]));
   }
 }
