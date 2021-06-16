620ceb9d44dde3de3b5180d33a799d366349d980
LWJerri <CLWJerri@yandex.ua>
2021-06-16 17:28:37 +0300

    Update base64Decoder file

--- apps/bot/src/helpers/base64Decoder.ts
+++ apps/bot/src/helpers/base64Decoder.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
 import { bot } from "../app";
+import { botLogger } from "./logger";
 
 export default async function base64Data(photo: string | any) {
-  const buffer = await axios.get((await bot.telegram.getFileLink(photo.file_id)).href, {
-    responseType: "arraybuffer",
-  });
+  try {
+    const buffer = await axios.get((await bot.telegram.getFileLink(photo.file_id)).href, {
+      responseType: "arraybuffer",
+    });
 
-  return `data:image/jpeg;base64,${Buffer.from(buffer.data, "binary").toString("base64")}`;
+    return `data:image/jpeg;base64,${Buffer.from(buffer.data, "binary").toString("base64")}`;
+  } catch (err) {
+    botLogger.error(`base64Decoder error:`, err.stack);
+  }
 }
