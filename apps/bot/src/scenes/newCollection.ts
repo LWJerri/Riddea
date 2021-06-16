a0afd6ece857d710fd4cc26afa67c6138ace71c7
LWJerri <CLWJerri@yandex.ua>
2021-06-16 17:30:20 +0300

    Update createcollection scene

--- apps/bot/src/scenes/newCollection.ts
+++ apps/bot/src/scenes/newCollection.ts
@@ -1,37 +1,53 @@
 import { Scenes } from "telegraf";
 import { getRepository } from "typeorm";
 import { Collection } from "@riddea/typeorm";
+import { botLogger } from "../helpers/logger";
 
 interface NewCollectionScene extends Scenes.SceneSessionData {
   collectionName: string;
 }
 
 export const newCollection = new Scenes.BaseScene<Scenes.SceneContext<NewCollectionScene>>("createCollection")
-  .enter((ctx) => ctx.reply("Enter the name of new collection"))
+  .enter((ctx) => {
+    try {
+      ctx.reply("Enter the name of new collection");
+    } catch (err) {
+      botLogger.error(`Scene newCollection error:`, err.stack);
+    }
+  })
   .on("text", async (ctx) => {
-    if (ctx.message.text.length > 15) {
-      return ctx.reply(`Oops! Collection name can be more than 15 symbols!`).catch(() => {});
+    try {
+      if (ctx.message.text.length > 15) {
+        return ctx.reply(`Oops! Collection name can be more than 15 symbols!`);
+      }
+
+      const repository = getRepository(Collection);
+      const isExists = await repository.findOne({
+        userID: ctx.from.id,
+        name: ctx.message.text,
+      });
+
+      if (isExists) {
+        return ctx.reply(`Collection with name ${isExists.name} already exists. Please, enter another name.`);
+      }
+
+      await repository.save({
+        name: ctx.message.text,
+        userID: ctx.from.id,
+        isPublic: false,
+      });
+
+      ctx.scene.session.collectionName = ctx.message.text;
+
+      await ctx.scene.leave();
+    } catch (err) {
+      botLogger.error(`Scene newCollection error:`, err.stack);
     }
-
-    const repository = getRepository(Collection);
-    const isExists = await repository.findOne({
-      userID: ctx.from.id,
-      name: ctx.message.text,
-    });
-
-    if (isExists) {
-      return ctx.reply(`Collection with name ${isExists.name} already exists. Please, enter another name.`).catch(() => {});
-    }
-
-    await repository.save({
-      name: ctx.message.text,
-      userID: ctx.from.id,
-      isPublic: false,
-    });
-    ctx.scene.session.collectionName = ctx.message.text;
-
-    ctx.scene.leave().catch(() => {});
   })
   .leave((ctx) => {
-    ctx.reply(`Collection with name ${ctx.scene.session.collectionName} created`).catch(() => {});
+    try {
+      ctx.reply(`Collection with name ${ctx.scene.session.collectionName} created`);
+    } catch (err) {
+      botLogger.error(`Scene newCollection error:`, err.stack);
+    }
   });
