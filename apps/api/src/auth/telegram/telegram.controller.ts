e577c8f8293a677eab3eded2b9cf9a8134b60ea8
LWJerri <CLWJerri@yandex.ua>
2021-06-16 17:20:07 +0300

    Update telegram service and controller

--- apps/api/src/auth/telegram/telegram.controller.ts
+++ apps/api/src/auth/telegram/telegram.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Response, Query, Request } from "@nestjs/common";
 import { ApiExcludeEndpoint } from "@nestjs/swagger";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { apiLogger } from "../../main";
 import { TelegramService } from "./telegram.service";
 
 @Controller("/v1/auth/telegram")
@@ -10,8 +11,12 @@ export class TelegramController {
   @ApiExcludeEndpoint()
   @Get("callback")
   async telegramCallback(@Query() query: any, @Request() req: FastifyRequest, @Response() res: FastifyReply) {
-    const user = await this.telegramService.confirmLogin(query);
-    req.session.set("user", user);
-    return res.redirect(302, "/");
+    try {
+      const user = await this.telegramService.confirmLogin(query);
+      req.session.set("user", user);
+      return res.redirect(302, "/");
+    } catch (err) {
+      return apiLogger.error(`Telegram Controller error:`, err.stack);
+    }
   }
 }
--- apps/api/src/auth/telegram/telegram.service.ts
+++ apps/api/src/auth/telegram/telegram.service.ts
@@ -10,26 +10,30 @@ type ConfirmLogin = SessionData["user"] & {
 @Injectable()
 export class TelegramService {
   async confirmLogin({ hash, ...userData }: ConfirmLogin) {
-    const botToken = process.env.TELEGRAM_BOT_TOKEN;
+    try {
+      const botToken = process.env.TELEGRAM_BOT_TOKEN;
 
-    if (!botToken) {
-      apiLogger.error("Bot token not setuped, telegram authorization will not work.");
-      throw new ServiceUnavailableException("Sorry, we have error on our side. Please contact site owners.");
-    }
+      if (!botToken) {
+        apiLogger.error("Bot token not setuped, telegram authorization will not work.");
+        throw new ServiceUnavailableException("Sorry, we have error on our side. Please contact with LWJerri (https://t.me/LWJerri).");
+      }
 
-    const secretKey = createHash("sha256").update(botToken).digest();
+      const secretKey = createHash("sha256").update(botToken).digest();
 
-    const dataCheckString = Object.keys(userData)
-      .sort()
-      .map((key) => `${key}=${userData[key]}`)
-      .join("\n");
+      const dataCheckString = Object.keys(userData)
+        .sort()
+        .map((key) => `${key}=${userData[key]}`)
+        .join("\n");
 
-    const hmac = createHmac("sha256", secretKey).update(dataCheckString).digest("hex");
+      const hmac = createHmac("sha256", secretKey).update(dataCheckString).digest("hex");
 
-    if (hmac === hash) {
-      return userData;
-    } else {
-      throw new UnauthorizedException("Hash missmatch.");
+      if (hmac === hash) {
+        return userData;
+      } else {
+        throw new UnauthorizedException("Hash missmatch.");
+      }
+    } catch (err) {
+      apiLogger.error(`Telegram service error:`, err.stack);
     }
   }
 }
