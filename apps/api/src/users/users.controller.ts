1b880bffb123b4066fa505b3c8e743efd4eaf7ac
LWJerri <CLWJerri@yandex.ua>
2021-06-16 17:30:55 +0300

    Update user controller

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { ApiResponse } from "@nestjs/swagger";
 import { FastifyRequest } from "fastify";
 import { CollectionsService } from "../collections/collections.service";
 import { CollectionDTO } from "../collections/dto/collection.dto";
+import { apiLogger } from "../main";
 
 @Controller("/v1/users")
 export class UsersController {
@@ -16,6 +17,10 @@ export class UsersController {
     type: [CollectionDTO],
   })
   getUserCollections(@Param("userId") userId: string, @Request() req: FastifyRequest) {
-    return this.collectionsService.getCollectionsByUser(userId, req.session);
+    try {
+      return this.collectionsService.getCollectionsByUser(userId, req.session);
+    } catch (err) {
+      apiLogger.error(`Users controller error:`, err.stack);
+    }
   }
 }
