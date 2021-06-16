b5b4bed3bddc887dcbc15f7d5eca147574de0828
LWJerri <CLWJerri@yandex.ua>
2021-06-16 17:27:42 +0300

    Update auth controller

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Request } from "@nestjs/common";
 import { ApiExcludeEndpoint } from "@nestjs/swagger";
 import { FastifyRequest } from "fastify";
+import { apiLogger } from "../main";
 
 @Controller("/v1/auth")
 export class AuthController {
@@ -13,6 +14,10 @@ export class AuthController {
   @ApiExcludeEndpoint()
   @Get("/logout")
   logout(@Request() req: FastifyRequest) {
-    return req.session.destroy();
+    try {
+      return req.session.destroy();
+    } catch (err) {
+      apiLogger.error(`Auth controller error:`, err.stack);
+    }
   }
 }
