be8cec64c76f10d0aa4a42b1a47efb22deac2892
LWJerri <CLWJerri@yandex.ua>
2021-06-16 13:35:50 +0300

    Move from reply_markup to Markup.inlineKeyboard in /start and /help commands
    fixes #70
    fixes #69

--- apps/bot/src/commands/help.ts
+++ apps/bot/src/commands/help.ts
@@ -1,4 +1,4 @@
-import { Context } from "telegraf";
+import { Context, Markup } from "telegraf";
 import { CommandInterface } from "./_interface";
 
 export default class extends CommandInterface {
@@ -12,25 +12,23 @@ export default class extends CommandInterface {
   }
 
   async run(ctx: Context) {
+    const keyboard = Markup.inlineKeyboard([
+      [{ text: "Sponsors", callback_data: "SEND_SPONSORS" }],
+      [
+        {
+          text: "GitHub",
+          url: "https://github.com/LWJerri/Riddea",
+        },
+        {
+          text: "Support me",
+          url: "https://www.donationalerts.com/r/lwjerri",
+        },
+      ],
+    ]);
+
     await ctx.reply(
       `Yo! Type / for view list of all bot commands.\n\nNOTE: If you wanna upload picture, you need send this picture to bot without commands, compression, and etc. Also you can forward picture from other chat :3`,
-      {
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: "Sponsors", callback_data: "SEND_SPONSORS" }],
-            [
-              {
-                text: "GitHub",
-                url: "https://github.com/LWJerri/Riddea",
-              },
-              {
-                text: "Support me",
-                url: "https://www.donationalerts.com/r/lwjerri",
-              },
-            ],
-          ],
-        },
-      },
+      keyboard,
     );
   }
 }
--- apps/bot/src/commands/start.ts
+++ apps/bot/src/commands/start.ts
@@ -1,4 +1,4 @@
-import { Context } from "telegraf";
+import { Context, Markup } from "telegraf";
 import { CommandInterface } from "./_interface";
 
 export default class extends CommandInterface {
@@ -10,31 +10,26 @@ export default class extends CommandInterface {
   }
 
   async run(ctx: Context) {
+    const keyboard = Markup.inlineKeyboard([
+      {
+        text: "Help menu",
+        callback_data: "SEND_HELPMENU",
+      },
+      {
+        text: "Statistics",
+        callback_data: "SEND_STATISTIC",
+      },
+      {
+        text: "GitHub",
+        url: "https://github.com/Riddea/Riddea",
+      },
+    ]);
+
     await ctx.reply(`Wuup! Hello, ${ctx.message.from.first_name} ＼(°o°)／`);
 
     await ctx.replyWithMarkdown(
-      `This bot provides a function for view random anime images and uploads your custom images. You can use command /help to view list of all commands or use the buttons to navigate on the bot.\nDeveloped by: [LWJerri](https://github.com/LWJerri)\nBig thanks to [Satont](https://github.com/Satont)\n\nNOTE: This bot have NSFW commands, check your room to stay in a safety ;)`,
-      {
-        disable_web_page_preview: true,
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: "Help menu",
-                callback_data: "SEND_HELPMENU",
-              },
-              {
-                text: "Statistics",
-                callback_data: "SEND_STATISTIC",
-              },
-              {
-                text: "GitHub",
-                url: "https://github.com/LWJerri/Riddea",
-              },
-            ],
-          ],
-        },
-      },
+      `This bot provides a function for view random anime images and uploads your custom images. You can use command /help to view list of all commands or use the buttons to navigate on the bot.\nDeveloped by [LWJerri](https://github.com/LWJerri)\nBig thanks to [Satont](https://github.com/Satont)\n\nNOTE: This bot have NSFW commands, check your room to stay in a safety ;)`,
+      keyboard,
     );
   }
 }
