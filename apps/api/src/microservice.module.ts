daf3f386f63b873ed1918db86d59dfcc41485e70
Satont <42675886+Satont@users.noreply.github.com>
2021-06-25 14:45:20 +0300

    Update microservice.module.ts

--- apps/api/src/microservice.module.ts
+++ apps/api/src/microservice.module.ts
@@ -1,13 +1,12 @@
 import { Global, Module } from "@nestjs/common";
 import { ClientsModule } from "@nestjs/microservices";
-import isDocker from "is-docker";
 
 const name = "BOT_SERVICE";
 const microserviceModule = ClientsModule.register([
   {
     name,
     options: {
-      host: isDocker() ? "bot" : process.env.BOT_SERVICE_HOST ?? "localhost",
+      host: process.env.BOT_SERVICE_HOST ?? "localhost",
       port: Number(process.env.BOT_PORT ?? 3001),
     },
   },
