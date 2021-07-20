88d757bb2826763249ab390c2fc2cb4626d63f59
LWJerri <CLWJerri@yandex.ua>
2021-07-20 16:09:51 +0300

    Prettier rules

--- packages/typeorm/src/migrations/1625063417236-user_settings.ts
+++ packages/typeorm/src/migrations/1625063417236-user_settings.ts
@@ -1,22 +1,21 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class userSettings1625063417236 implements MigrationInterface {
-    name = 'userSettings1625063417236'
+  name = "userSettings1625063417236";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "IDX_4c1989542e47d9e3b98fe32c67"`);
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "userID" integer NOT NULL, "lang" character varying NOT NULL, "startedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "sessions" ALTER COLUMN "expireAt" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "sessions" ALTER COLUMN "expireAt" SET DEFAULT '1625063417837'`);
-        await queryRunner.query(`CREATE INDEX "IDX_23b879ac118a273f7d4510cc6d" ON "sessions" ("expireAt") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "userID" integer NOT NULL, "lang" character varying NOT NULL, "startedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(`ALTER TABLE "sessions" ALTER COLUMN "expireAt" SET NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "sessions" ALTER COLUMN "expireAt" SET DEFAULT '1625063417837'`);
+    await queryRunner.query(`CREATE INDEX "IDX_23b879ac118a273f7d4510cc6d" ON "sessions" ("expireAt") `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "IDX_23b879ac118a273f7d4510cc6d"`);
-        await queryRunner.query(`ALTER TABLE "sessions" ALTER COLUMN "expireAt" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "sessions" ALTER COLUMN "expireAt" DROP NOT NULL`);
-        await queryRunner.query(`DROP TABLE "user"`);
-        await queryRunner.query(`CREATE INDEX "IDX_4c1989542e47d9e3b98fe32c67" ON "sessions" ("expireAt") `);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "IDX_23b879ac118a273f7d4510cc6d"`);
+    await queryRunner.query(`ALTER TABLE "sessions" ALTER COLUMN "expireAt" DROP DEFAULT`);
+    await queryRunner.query(`ALTER TABLE "sessions" ALTER COLUMN "expireAt" DROP NOT NULL`);
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
 }
