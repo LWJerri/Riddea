5ab2af28602ec94ad729f4b7e2ab90a8dca272b5
LWJerri <CLWJerri@yandex.ua>
2021-07-21 22:12:39 +0300

    Register new stage

--- apps/bot/src/constants/stages.ts
+++ apps/bot/src/constants/stages.ts
@@ -2,5 +2,6 @@ import { Scenes } from "telegraf";
 import { myImages } from "../scenes/myImages";
 import { newCollection } from "../scenes/newCollection";
 import { uploadScene } from "../scenes/upload";
+import { settingScene } from "../scenes/setting";
 
-export const stage = new Scenes.Stage<Scenes.SceneContext>([uploadScene, myImages, newCollection]);
+export const stage = new Scenes.Stage<Scenes.SceneContext>([uploadScene, myImages, newCollection, settingScene]);
