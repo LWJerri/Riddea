ba442a3548e4432b2d5b13a714f0a8e25b9dbafa
LWJerri <CLWJerri@yandex.ua>
2021-10-20 18:12:21 +0300

    Add new scene to scene list

--- apps/backend/src/bot/constants/stages.ts
+++ apps/backend/src/bot/constants/stages.ts
@@ -1,8 +1,9 @@
 import { Scenes } from "telegraf";
+import { imageEdit } from "../scenes/imageEdit";
 
 import { myImages } from "../scenes/myImages";
 import { newCollection } from "../scenes/newCollection";
 import { settingScene } from "../scenes/setting";
 import { uploadScene } from "../scenes/upload";
 
-export const stage = new Scenes.Stage<Scenes.SceneContext>([uploadScene, myImages, newCollection, settingScene]);
+export const stage = new Scenes.Stage<Scenes.SceneContext>([uploadScene, myImages, newCollection, settingScene, imageEdit]);
