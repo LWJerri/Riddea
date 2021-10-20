6adc61960f3e00e016c3997de42be3b6232a9b6a
LWJerri <CLWJerri@yandex.ua>
2021-10-20 22:06:32 +0300

    Register new renameCollection scene

--- apps/backend/src/bot/constants/stages.ts
+++ apps/backend/src/bot/constants/stages.ts
@@ -3,7 +3,15 @@ import { imageEdit } from "../scenes/imageEdit";
 
 import { myImages } from "../scenes/myImages";
 import { newCollection } from "../scenes/newCollection";
+import { renameCollection } from "../scenes/renameCollection";
 import { settingScene } from "../scenes/setting";
 import { uploadScene } from "../scenes/upload";
 
-export const stage = new Scenes.Stage<Scenes.SceneContext>([uploadScene, myImages, newCollection, settingScene, imageEdit]);
+export const stage = new Scenes.Stage<Scenes.SceneContext>([
+  uploadScene,
+  myImages,
+  newCollection,
+  settingScene,
+  imageEdit,
+  renameCollection,
+]);
