ce2e13eedbb61649e0e799e1709b78fb3a3ec40f
LWJerri <CLWJerri@yandex.ua>
2021-10-21 20:41:58 +0300

    Add new command - yuri

---
+++ apps/backend/src/bot/commands/yuri.ts
@@ -0,0 +1,51 @@
+import { Markup } from "telegraf";
+import { nekosLifeApi } from "../helpers/nekosLifeApi";
+import { ContextCallbackWithData } from "../typings/telegraf";
+import { CommandInterface } from "./_interface";
+
+export default class extends CommandInterface {
+  constructor() {
+    super({
+      description: "[NSFW]: Send yuri images",
+      collectUsage: true,
+      cooldown: true,
+      name: "yuri",
+      actions: [
+        {
+          name: "NekosLife Service [NSFW]",
+          callback: "NEW_YURI_NEKOS",
+        },
+      ],
+    });
+  }
+
+  async run(ctx: ContextCallbackWithData) {
+    const сallback = ctx.callbackQuery?.data;
+
+    const keyboard = Markup.inlineKeyboard(
+      this.actions.map((action) => Markup.button.callback(action.name, action.callback)),
+      { columns: 1 },
+    );
+
+    async function API(callback?: string) {
+      if (!callback || callback == "NEW_YURI_NEKOS") {
+        const endpoints = ["yuri", "eroyuri"];
+
+        return await nekosLifeApi({ endPoint: endpoints[Math.floor(Math.random() * endpoints.length)], amount: 10 });
+      }
+    }
+
+    const images = await API(сallback);
+
+    await ctx.replyWithMediaGroup(
+      images.map((image) => {
+        return {
+          type: "photo",
+          media: image,
+        };
+      }),
+    );
+
+    await ctx.reply(ctx.i18n.translate("bot.main.newPack.images", { pack: ctx.i18n.translate("bot.packs.yuri") }), keyboard);
+  }
+}
