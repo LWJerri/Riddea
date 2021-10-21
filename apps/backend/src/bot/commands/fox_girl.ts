22170497dac98365082bc63db6c5247684786e3c
LWJerri <CLWJerri@yandex.ua>
2021-10-21 20:42:25 +0300

    Add new command - foxgirl

---
+++ apps/backend/src/bot/commands/fox_girl.ts
@@ -0,0 +1,47 @@
+import { Markup } from "telegraf";
+import { nekosLifeApi } from "../helpers/nekosLifeApi";
+import { ContextCallbackWithData } from "../typings/telegraf";
+import { CommandInterface } from "./_interface";
+
+export default class extends CommandInterface {
+  constructor() {
+    super({
+      description: "Send foxgirl images",
+      collectUsage: true,
+      cooldown: true,
+      name: "foxgirl",
+      actions: [
+        {
+          name: "NekosLife Service",
+          callback: "NEW_FOX_NEKOS",
+        },
+      ],
+    });
+  }
+
+  async run(ctx: ContextCallbackWithData) {
+    const сallback = ctx.callbackQuery?.data;
+
+    const keyboard = Markup.inlineKeyboard(
+      this.actions.map((action) => Markup.button.callback(action.name, action.callback)),
+      { columns: 1 },
+    );
+
+    async function API(callback?: string) {
+      if (!callback || callback == "NEW_FOX_NEKOS") return await nekosLifeApi({ endPoint: "fox_girl", amount: 10 });
+    }
+
+    const images = await API(сallback);
+
+    await ctx.replyWithMediaGroup(
+      images.map((image) => {
+        return {
+          type: "photo",
+          media: image,
+        };
+      }),
+    );
+
+    await ctx.reply(ctx.i18n.translate("bot.main.newPack.images", { pack: ctx.i18n.translate("bot.packs.fox_girl") }), keyboard);
+  }
+}
